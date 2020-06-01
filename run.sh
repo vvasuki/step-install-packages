749b9f3c269dbea5ce2016e42ed9e9a11f560da8
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2020-06-02 04:33:23 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -25,9 +25,8 @@ exec_install_packages(){
   mkdir -p $APT_LIST_PATH
   sudo rm -fr /var/lib/apt/lists
   sudo ln -s $APT_LIST_PATH/ /var/lib/apt/lists
-  if [ $( find $WERCKER_CACHE_DIR/wercker/aptupdated -mtime -1 | wc -l ) -eq 0 ] ;
-  then sudo apt-get update; touch $WERCKER_CACHE_DIR/wercker/aptupdated;  fi
-
+  sudo add-apt-repository universe 
+  sudo apt-get update
   sudo apt-get install $WERCKER_INSTALL_PACKAGES_PACKAGES -y
 
   if [[ $? -ne 0 ]]; then
